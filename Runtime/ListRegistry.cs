268ec5563b464b5c7bd8b46846dda385f89c4e81
forcepusher <bionitsoup@gmail.com>
2025-06-22 23:50:02 +0700

    Fix bug and improve readability

--- Runtime/ListRegistry.cs
+++ Runtime/ListRegistry.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using UnityEngine;
 
 namespace BananaParty.Registry
@@ -11,12 +10,12 @@ namespace BananaParty.Registry
         private bool _neverUnload = false;
 
         private readonly List<T> _entries = new();
-        private readonly Dictionary<T, int> _entriesLookup = new();
+        private readonly Dictionary<T, int> _entriesIndexLookupTable = new();
 
         private void OnEnable()
         {
             _entries.Clear();
-            _entriesLookup.Clear();
+            _entriesIndexLookupTable.Clear();
 
             if (_neverUnload)
                 hideFlags |= HideFlags.DontUnloadUnusedAsset;
@@ -29,7 +28,7 @@ namespace BananaParty.Registry
             if (entry == null)
                 throw new ArgumentNullException(nameof(entry));
 
-            if (_entriesLookup.TryAdd(entry, _entries.Count))
+            if (_entriesIndexLookupTable.TryAdd(entry, _entries.Count))
                 _entries.Add(entry);
             else
                 throw new InvalidOperationException($"Attempt to {nameof(AddEntry)} {nameof(T)} that already exists in {nameof(ListRegistry<T>)}.");
@@ -40,11 +39,18 @@ namespace BananaParty.Registry
             if (entry == null)
                 throw new ArgumentNullException(nameof(entry));
 
-            if (_entriesLookup.Remove(entry, out int index))
+            if (_entriesIndexLookupTable.Remove(entry, out int entryIndexToRemove))
             {
-                _entries[index] = _entries[^1];
-                _entriesLookup[_entries[^1]] = index;
-                _entries.RemoveAt(_entries.Count - 1);
+                int lastEntryIndex = _entries.Count - 1;
+
+                if (entryIndexToRemove < lastEntryIndex)
+                {
+                    T lastEntry = _entries[lastEntryIndex];
+                    _entries[entryIndexToRemove] = lastEntry;
+                    _entriesIndexLookupTable[lastEntry] = entryIndexToRemove;
+                }
+
+                _entries.RemoveAt(lastEntryIndex);
             }
             else
             {
