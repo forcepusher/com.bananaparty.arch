92962cc02d68e0700b476657a5084362fb3db637
forcepusher <bionitsoup@gmail.com>
2025-07-03 04:41:06 +0700

    Add SceneReference

---
+++ Runtime/SceneReference.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BananaParty.Registry
+{
+    [Serializable]
+    public class SceneReference
+    {
+#if UNITY_EDITOR
+        [SerializeField]
+        internal SceneAsset _sceneAsset;
+#endif
+        [SerializeField]
+        internal string _scenePath;
+
+        public string ScenePath => _scenePath;
+        public string SceneName => System.IO.Path.GetFileNameWithoutExtension(ScenePath);
+    }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(SceneReference))]
+    public class SceneReferencePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty sceneAssetProperty = property.FindPropertyRelative(nameof(SceneReference._sceneAsset));
+            SerializedProperty scenePathProperty = property.FindPropertyRelative(nameof(SceneReference._scenePath));
+
+            SceneAsset sceneAsset = sceneAssetProperty.objectReferenceValue as SceneAsset;
+            if (sceneAsset != null)
+            {
+                string fullPath = AssetDatabase.GetAssetPath(sceneAsset);
+                string prefix = "Assets/";
+                string suffix = ".unity";
+                scenePathProperty.stringValue = fullPath.Substring(prefix.Length, fullPath.Length - prefix.Length - suffix.Length);
+            }
+
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+    }
+#endif
+}
